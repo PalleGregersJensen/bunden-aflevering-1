6d29d830ab1a6012f0511260b7aa036fe39e2cb3
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-09-01 13:59:59 +0200

    Backend henter JSON-data fra backend. Mangler at få koblet backend og frontend sammen.

--- Backend/app-backend.js
+++ Backend/app-backend.js
@@ -1,13 +1,25 @@
 import express, { response } from "express";
+import fs from "fs/promises";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`App is running on http://localhost:${port}`);
-}) 
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (request, response)  => {
-    response.send("Hello world");
-}
-)
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
+
+app.get("/", (request, response) => {
+    response.send("My artists endpoint");
+    console.log("læses dette");
+});
+
+app.get("/artists", async (request, response) => {
+    const data = await fs.readFile("artists.json");
+    const artists = JSON.parse(data);
+    console.log(artists);
+    response.json(artists);
+});
