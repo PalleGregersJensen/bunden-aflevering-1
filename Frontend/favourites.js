030018a008485044e0510fe6f0525ad8068a606b
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-09-04 23:22:05 +0200

    remove from favourites virker ikke helt endnu.  localStorage skal også indtænkes i den del.

--- Frontend/favourites.js
+++ Frontend/favourites.js
@@ -1,11 +1,14 @@
 import { favourites, endpoint } from "./app.js";
 
+let newFavourites = [];
+
 // Add artist to favourites and push to favourites-array
 function addArtistToFavourites(artistObject) {
   console.log("læses dette");
   console.log(artistObject);
   favourites.push(artistObject);
   console.log(favourites);
+  // lav favourites om til string
   let favouritesAsString = JSON.stringify(favourites);
   console.log(favourites);
   localStorage.setItem("favouritesToBeStored", favouritesAsString);
@@ -17,16 +20,38 @@ function showFavouriteArtists() {
   document.querySelector("#show-favourites-button").classList.add("hidden");
   document.querySelector("#show-all-artists-button").classList.remove("hidden");
   let favouritesAsString = localStorage.getItem("favouritesToBeStored");
-  let newfavourites = JSON.parse(favouritesAsString);
-  showFavouritesOnWebsite(newfavourites);
+  // lav favourites om til objekter i array igen
+  newFavourites = JSON.parse(favouritesAsString);
+    newFavourites.sort((a, b) => a.name.localeCompare(b.name));
+    console.log(newFavourites);
+  showFavouritesOnWebsite(newFavourites);
 }
 
+// loop through favourites-array and displayon website
 function showFavouritesOnWebsite(artistList) {
   document.querySelector("#artist-list").innerHTML = "";
   for (const artist of artistList) {
-    const favouritesHtml = /*html*/ `<div>Name: ${artist.name} <br> Active since: ${artist.activeSince} <br> <img src="${artist.image}"/> <br> <button class="remove-artist-from-fvourites-button">Remove from favourites</button> <br></button></div></button>`;
+    const favouritesHtml = /*html*/ `<div>Name: ${artist.name} <br> Active since: ${artist.activeSince} <br> <img src="${artist.image}"/> <br> <button class="remove-artist-from-favourites-button">Remove from favourites</button> <br></button></div></button>`;
     document.querySelector("#artist-list").insertAdjacentHTML("beforeend", favouritesHtml);
+
+    // add to fovurites
+    document
+      .querySelector("#artist-list div:last-child .remove-artist-from-favourites-button")
+      .addEventListener("click", () => removeArtistFromNewFavourites(artist));
   }
+}
+
+// remove artist from favourites
+function removeArtistFromNewFavourites(artistObject) {
+    console.log(artistObject);
+    const artistToBeRemoved = newFavourites.indexOf(artistObject);
+    console.log(artistToBeRemoved);
+    const artistToBeRemovedSeceondPosition = artistToBeRemoved;
+    console.log(artistToBeRemovedSeceondPosition);
+    const removedArtist = newFavourites.splice(artistToBeRemoved, artistToBeRemovedSeceondPosition);
+    console.log(removedArtist);
+    console.log(newFavourites);
+    showFavouritesOnWebsite(newFavourites);
 }
 
 export { addArtistToFavourites, showFavouriteArtists };
